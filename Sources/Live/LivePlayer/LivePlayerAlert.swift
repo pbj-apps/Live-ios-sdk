571cfe6c71256daa695cac453f75a8cf366da890
sachadso@gmail.com <sachadso@gmail.com>
2021-05-04 19:23:28 +0200

    fix(indentation): fixes indentation

--- Sources/Live/LivePlayer/LivePlayerAlert.swift
+++ Sources/Live/LivePlayer/LivePlayerAlert.swift
@@ -12,7 +12,7 @@ import Combine
 // Source https://stackoverflow.com/questions/60900554/swiftui-binding-default-value-argument-labels-wrappedvalue-do-not-match-an
 
 class TextFieldAlertViewController: UIViewController {
-	
+
 	/// Presents a UIAlertController (alert style) with a UITextField and a `Done` button
 	/// - Parameters:
 	///   - title: to be used as title of the UIAlertController
@@ -26,31 +26,31 @@ class TextFieldAlertViewController: UIViewController {
 		self.isPresented = isPresented
 		super.init(nibName: nil, bundle: nil)
 	}
-	
+
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	
+
 	// MARK: - Dependencies
 	private let alertTitle: String
 	private let message: String?
 	@Binding private var text: String?
 	private var isPresented: Binding<Bool>?
-	
+
 	// MARK: - Private Properties
 	private var subscription: AnyCancellable?
-	
+
 	// MARK: - Lifecycle
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		presentAlertController()
 	}
-	
+
 	private func presentAlertController() {
 		guard subscription == nil else { return } // present only once
-		
+
 		let vc = UIAlertController(title: alertTitle, message: message, preferredStyle: .alert)
-		
+
 		// add a textField and create a subscription to update the `text` binding
 		vc.addTextField { [weak self] textField in
 			guard let self = self else { return }
@@ -59,7 +59,7 @@ class TextFieldAlertViewController: UIViewController {
 				.map { ($0.object as? UITextField)?.text }
 				.assign(to: \.text, on: self)
 		}
-		
+
 		// create a `Done` action that updates the `isPresented` binding when tapped
 		// this is just for Demo only but we should really inject
 		// an array of buttons (with their title, style and tap handler)
@@ -72,13 +72,13 @@ class TextFieldAlertViewController: UIViewController {
 }
 
 struct TextFieldAlert {
-	
+
 	// MARK: Properties
 	let title: String
 	let message: String?
 	@Binding var text: String?
 	var isPresented: Binding<Bool>? = nil
-	
+
 	// MARK: Modifiers
 	func dismissable(_ isPresented: Binding<Bool>) -> TextFieldAlert {
 		TextFieldAlert(title: title, message: message, text: $text, isPresented: isPresented)
@@ -86,13 +86,13 @@ struct TextFieldAlert {
 }
 
 extension TextFieldAlert: UIViewControllerRepresentable {
-	
+
 	typealias UIViewControllerType = TextFieldAlertViewController
-	
+
 	func makeUIViewController(context: UIViewControllerRepresentableContext<TextFieldAlert>) -> UIViewControllerType {
 		TextFieldAlertViewController(title: title, message: message, text: $text, isPresented: isPresented)
 	}
-	
+
 	func updateUIViewController(_ uiViewController: UIViewControllerType,
 															context: UIViewControllerRepresentableContext<TextFieldAlert>) {
 		// no update needed
@@ -100,11 +100,11 @@ extension TextFieldAlert: UIViewControllerRepresentable {
 }
 
 struct TextFieldWrapper<PresentingView: View>: View {
-	
+
 	@Binding var isPresented: Bool
 	let presentingView: PresentingView
 	let content: () -> TextFieldAlert
-	
+
 	var body: some View {
 		ZStack {
 			if (isPresented) { content().dismissable($isPresented) }
