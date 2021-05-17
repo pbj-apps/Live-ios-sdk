930df2f39654d2158ed29d55389e4812c2fda92b
sachadso@gmail.com <sachadso@gmail.com>
2021-05-17 21:12:39 +0200

    fix(videoPlayer): Reload player when failed to fix mux warmup time

--- Sources/Live/LivePlayer/VideoPlayer.swift
+++ Sources/Live/LivePlayer/VideoPlayer.swift
@@ -29,33 +29,16 @@ public struct VideoPlayer: UIViewRepresentable {
 	public func makeUIView(context: Context) -> UIView {
 		let playerView = VideoAVPlayerView()
 		playerView.playerLayer?.videoGravity = .resizeAspectFill
-		if let assetURL = URL(string: url) {
-			let asset = AVAsset(url: assetURL)
-			let playerItem = AVPlayerItem(asset: asset)
-
-			if isLive {
-				playerItem.automaticallyPreservesTimeOffsetFromLive = true
-				playerItem.canUseNetworkResourcesForLiveStreamingWhilePaused = true
-			}
-
-			let player = AVQueuePlayer(playerItem: playerItem)
-			if looping {
-				context.coordinator.looper = AVPlayerLooper(player: player, templateItem: playerItem)
-			}
-			playerView.player = player
-			context.coordinator.player = player
-		}
+		context.coordinator.loadPlayer(url: url, in: playerView, isLooping: looping, isLive: isLive)
 		return playerView
 	}
 
 	public func updateUIView(_ uiView: UIView, context: Context) {
 		if isPlaying {
-
 			if isLive {
 				// Keep close to direct as much as possible.
 				context.coordinator.player?.seek(to: CMTime.positiveInfinity)
 			}
-
 			context.coordinator.player?.play()
 		} else {
 			context.coordinator.player?.pause()
@@ -67,9 +50,87 @@ public struct VideoPlayer: UIViewRepresentable {
 		return Coordinator()
 	}
 
-	public class Coordinator {
+	public class Coordinator: NSObject {
+
+		var url: String = ""
 		var looper: AVPlayerLooper?
 		var player: AVPlayer?
+		var playerView: VideoAVPlayerView?
+		var isLooping: Bool = false
+		var isLive: Bool = false
+		var playerItemContext = 0
+
+		func loadPlayer(url: String, in playerView: VideoAVPlayerView, isLooping: Bool, isLive: Bool) {
+			self.url = url
+			self.playerView = playerView
+			self.isLooping = isLooping
+			self.isLive = isLive
+			self.reloadPlayer()
+		}
+
+		func reloadPlayer() {
+			if let assetURL = URL(string: url) {
+				let asset = AVAsset(url: assetURL)
+				let playerItem = AVPlayerItem(asset: asset)
+
+				if isLive {
+					playerItem.automaticallyPreservesTimeOffsetFromLive = true
+					playerItem.canUseNetworkResourcesForLiveStreamingWhilePaused = true
+
+					// Register as an observer of the player item's status property
+					playerItem.addObserver(self,
+																		 forKeyPath: #keyPath(AVPlayerItem.status),
+																		 options: [.old, .new],
+																		 context: &playerItemContext)
+				}
+
+				if isLooping {
+					let queuePlayer = AVQueuePlayer(playerItem: playerItem)
+					looper = AVPlayerLooper(player: queuePlayer, templateItem: playerItem)
+					player = queuePlayer
+				} else {
+					player = AVPlayer(playerItem: playerItem)
+					player?.automaticallyWaitsToMinimizeStalling = true
+				}
+				playerView?.player = player
+			}
+		}
+
+		public override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+
+			//Only handle observations for the playerItemContext
+			guard context == &playerItemContext else {
+				super.observeValue(forKeyPath: keyPath,
+													 of: object,
+													 change: change,
+													 context: context)
+				return
+			}
+
+			if keyPath == #keyPath(AVPlayerItem.status) {
+				let status: AVPlayerItem.Status
+
+				// Get the status change from the change dictionary
+				if let statusNumber = change?[.newKey] as? NSNumber {
+					status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+				} else {
+					status = .unknown
+				}
+
+				// Switch over the status
+				switch status {
+				case .readyToPlay:
+					player?.play()
+				case .failed:
+					// Typical case is that Livestream just started and not ready to play yet.
+					// Retry 5s later.
+					Timer.scheduledTimer(withTimeInterval: 3, repeats: false) { _ in
+						self.reloadPlayer()
+					}
+				case .unknown:()
+				}
+			}
+		}
 	}
 }
 
